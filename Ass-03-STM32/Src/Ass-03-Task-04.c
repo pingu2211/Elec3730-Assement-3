5db7ace88960257939cd54ae1fbddaac79cf3f50
William-Tweedale <38878847+William-Tweedale@users.noreply.github.com>
2018-06-26 14:58:26 +1000

    commented the shit out of this thing

--- Ass-03-STM32/Src/Ass-03-Task-04.c
+++ Ass-03-STM32/Src/Ass-03-Task-04.c
@@ -151,7 +151,7 @@ uint8_t std_dev(double *inp_array, int array_size){
 /*******************************************************************************************************/
 /* function for downsampling the input data */
 uint8_t downsampling(double *inp_array, int array_size, int ds_factor){
-	int i;								// downsampling factor, eg. if ds_factor = 100, function would make new array with every hundredth element of the input array
+	int i;										// downsampling factor, eg. if ds_factor = 100, function would make new array with every hundredth element of the input array
 	double *new_array= malloc(sizeof(double)*ds_factor);
 	for(i=0;i<array_size;i+=ds_factor){
 		new_array[i/ds_factor] = inp_array[i];	// new array downsampled from input array by downsampling factor, eg. if ds_factor = 100, function would make new array with every hundredth element of the input array
@@ -161,99 +161,99 @@ uint8_t downsampling(double *inp_array, int array_size, int ds_factor){
 /*******************************************************************************************************/
 /* function that reads array from file */
 uint8_t pull_array(char *filename, double *inp_array){
-	FIL fobj;
-	FRESULT res;
-	int time, size;
-	int byte_size = sizeof(int);
-	int array_size_bytes, array_size;
+	FIL fobj;														// file object
+	FRESULT res;													// result of FATFS functions -> FR_OK = 0 = success
+	int time, size;													// data info: time length of sample, size of array
+	int byte_size = sizeof(int);									// number of bytes to be read. Initialized as size of integer
+	int array_size_bytes, array_size;								// size of array read from file. array_size = num of array elements, array_size_bytes = num of bytes
 
 	/* open file */
-	res = f_open(&fobj, filename, FA_READ);
-	if (!res){
-		safe_printf("%s failed to open.\n\r", filename);
+	res = f_open(&fobj, filename, FA_READ);							// FATFS function opens file for reading
+	if (!res){														// if function failed
+		safe_printf("%s failed to open.\n\r", filename);			// print error
 		return -1;
 	}
-	/* time*/
-	res = f_read(&fobj, &time, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("%s could not be read.\n\r", filename);
+	/* reading time*/												// if function was successful read the file with FATFS function
+	res = f_read(&fobj, &time, byte_size, &array_size_bytes);		// read first element of file = time
+	if(!res){														// if function failed
+		safe_printf("%s could not be read.\n\r", filename);			// print error
 		return -1;
 	}
-	if (byte_size != array_size_bytes){
-		safe_printf("%s could not be read.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare bytes predicted to be read to bytes actually read
+		safe_printf("%s could not be read.\n\r", filename);			// if not same print error
 		return -1;
 	}
-	/* array size */
-	res = f_read(&fobj, &size, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("%s could not be read.\n\r", filename);
+	/* reading array size */
+	res = f_read(&fobj, &size, byte_size, &array_size_bytes);		// read second element of file = array size
+	if(!res){														// if function failed
+		safe_printf("%s could not be read.\n\r", filename);			// print error
 		return -1;
 	}
-	if (byte_size != array_size_bytes){
-		safe_printf("%s could not be read.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare bytes predicted to be read to bytes actually read
+		safe_printf("%s could not be read.\n\r", filename);			// if not same print error
 		return -1;
 	}
 	/* reading array */
-	byte_size = size*sizeof(double);
-	res = f_read(&fobj, inp_array, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("%s could not be read.\n\r", filename);
+	byte_size = size*sizeof(double);								// predicted byte size of array = size of array times the size of double
+	res = f_read(&fobj, inp_array, byte_size, &array_size_bytes);	// read third element of the file = array of data
+	if(!res){														// if function failed
+		safe_printf("%s could not be read.\n\r", filename);			// print error
 		return -1;
 	}
-	if (byte_size != array_size_bytes){
-		safe_printf("%s could not be read.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare bytes predicted to be read to bytes actually read
+		safe_printf("%s could not be read.\n\r", filename);			// if not same print error
 		return -1;
 	}
-	array_size = array_size_bytes/sizeof(double);
-	f_close(&fobj);
-	return array_size;
+	array_size = array_size_bytes/sizeof(double);					// array_size_ bytes = num bytes in array. Convert to array_size = num of elements in array
+	f_close(&fobj);													// FATFS function closes file
+	return array_size;												// return num of elements in array
 }
 /*******************************************************************************************************/
 /* function that reads array from file */
 uint8_t write_to_file(char *filename, double *inp_array, int time, int size){
-	FIL fobj;
-	FRESULT res;
-	int byte_size = sizeof(int);
-	int array_size_bytes, array_size;
+	FIL fobj;														// file object
+	FRESULT res;													// result of FATFS functions -> FR_OK = 0 = success
+	int byte_size = sizeof(int);									// number of bytes to be read. Initialized as size of integer
+	int array_size_bytes, array_size;								// size of array read from file. array_size = num of array elements, array_size_bytes = num of bytes
 
 	/* open file */
-	res = f_open(&fobj, filename, FA_READ);
-	if (!res){
-		safe_printf("%s failed to open.\n\r", filename);
+		res = f_open(&fobj, filename, FA_READ);						// FATFS function opens file for reading
+		if (!res){													// if function failed
+			safe_printf("%s failed to open.\n\r", filename);		// print error
+			return -1;
+		}
+	/* writing time to file */										// FATFS function writes to file
+	res = f_write(&fobj, &time, byte_size, &array_size_bytes);		// write time to first element in file
+	if(!res){														// if function failed
+		safe_printf("Could not write to %s.\n\r", filename);		// print error
 		return -1;
 	}
-	/* writing time to file */
-	res = f_write(&fobj, &time, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("Could not write to %s.\n\r", filename);
-		return -1;
-	}
-	if (byte_size != array_size_bytes){
-		safe_printf("Could not write to %s.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare predicted amount of bytes to be written to actual amount written
+		safe_printf("Could not write to %s.\n\r", filename);		// if not same print error
 		return -1;
 	}
 	/* writing array size to file */
-	res = f_write(&fobj, &size, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("Could not write to %s.\n\r", filename);
+	res = f_write(&fobj, &size, byte_size, &array_size_bytes);		// writes array size to second file elemenet
+	if(!res){														// if function failed
+		safe_printf("Could not write to %s.\n\r", filename);		// print error
 		return -1;
 	}
-	if (byte_size != array_size_bytes){
-		safe_printf("Could not write to %s.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare predicted amount of bytes to be written to actual amount written
+		safe_printf("Could not write to %s.\n\r", filename);		// if not same print error
 		return -1;
 	}
 	/* writing array to file */
-	byte_size = size*sizeof(double);
-	res = f_write(&fobj, inp_array, byte_size, &array_size_bytes);
-	if(!res){
-		safe_printf("Could not write to file %s.\n\r", filename);
+	byte_size = size*sizeof(double);								// predicted byte size of array = size of array times the size of double
+	res = f_write(&fobj, inp_array, byte_size, &array_size_bytes);	// write array to third element in file
+	if(!res){														// if function failed
+		safe_printf("Could not write to file %s.\n\r", filename);	// print error
 		return -1;
 	}
-	if (byte_size != array_size_bytes){
-		safe_printf("Could not write to file %s.\n\r", filename);
+	if (byte_size != array_size_bytes){								// compare predicted amount of bytes to be written to actual amount written
+		safe_printf("Could not write to file %s.\n\r", filename);	// if not same print error
 		return -1;
 	}
-	array_size = array_size_bytes/sizeof(double);
-	f_close(&fobj);
-	return array_size;
+	array_size = array_size_bytes/sizeof(double);					// array size in bytes converted to array size = num of elements
+	f_close(&fobj);													// FATFS function closes file
+	return array_size;												// returns num of elements in array stored in file
 }
